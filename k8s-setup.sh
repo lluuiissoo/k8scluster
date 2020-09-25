5eb0dab8b95db49e1a93c77038c78ccf29124d1e
lluuiissoo <lluuiissoo@gmail.com>
2020-09-25 16:13:56 -0400

    Updating hostname

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -1,4 +1,14 @@
+# Kudos to https://www.youtube.com/watch?v=UWg3ORRRF60&list=LL6Xw7-4WmPUNltIeoeMiyWQ&index=7
+
 # Update repositories
 #sudo su
 apt-get update
 
+# Turn off Swap space
+#swapoff -a
+#nano /etc/fstab #Comment line containing swap, if it exists
+
+# Update hostname
+echo "Enter hostname:"
+read hostName
+echo hostName > /etc/hostname
