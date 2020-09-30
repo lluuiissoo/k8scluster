0d432a28be239a2116f80035642d5432d3220d35
lluuiissoo <lluuiissoo@gmail.com>
2020-09-30 17:33:19 -0400

    Typo

--- enable-cgroups.sh
+++ enable-cgroups.sh
@@ -16,7 +16,7 @@
 
 # Create or replace the contents of /etc/docker/daemon.json to enable the systemd cgroup driver
 
-$ sudo cat > /etc/docker/daemon.json <<EOF
+sudo cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
@@ -30,4 +30,4 @@ EOF
 # Enable cgroups limit support
 # Append the cgroups and swap options to the kernel command line
 # Note the space before "cgroup_enable=cpuset", to add a space after the last existing item on the line
-$ sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
+sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
