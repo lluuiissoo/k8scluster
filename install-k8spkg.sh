d0e9f1cf4c2764dda095e49b37145d3916f4eb2a
lluuiissoo <lluuiissoo@gmail.com>
2020-10-03 16:27:01 +0000

    Install

--- install-k8spkg.sh
+++ install-k8spkg.sh
@@ -10,3 +10,13 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+
+# Update the apt cache and install kubelet, kubeadm, and kubectl
+# (Output omitted)
+sudo apt update && sudo apt install -y kubelet kubeadm kubectl
+
+# Disable (mark as held) updates for the Kubernetes packages
+sudo apt-mark hold kubelet kubeadm kubectl
+#kubelet set on hold.
+#kubeadm set on hold.
+#kubectl set on hold.
